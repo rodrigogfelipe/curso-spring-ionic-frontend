2db5973d5ee375c75e5dd78d6475a179d9441d93
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-12-06 11:50:20 -0300

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -10,6 +10,10 @@ export class ClienteService {
     constructor(public http: HttpClient, public storage: StorageService) {
 }
 
+findById(id: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+}
+
 /*Metado findByEmail recebe o email e o ClienteDTO */ 
 findByEmail(email: string) {
     return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
