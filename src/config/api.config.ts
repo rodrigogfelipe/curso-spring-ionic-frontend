06f6c9d3ddae2a08dee49df4237218bb04aec29f
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-12-23 00:15:25 -0300

    Usando cordova / Fazendo upload da foto - Validados

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 /*Configurando a URL do spring boot*/
 /*bucketBaseUrl e a URL do endereço do bucket amazon*/ 
 export const API_CONFIG = {
-    baseUrl: "https://curso-spring-ionic-web.herokuapp.com",
+    baseUrl: "http://localhost:8080",
     bucketBaseUrl: "https://curso-spring-ionic-web.s3-sa-east-1.amazonaws.com"
 }
